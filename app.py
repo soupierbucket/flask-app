96c789bcc0428ff0eb53ac6dddf473556f815c0c
soupierbucket <42323328+soupierbucket@users.noreply.github.com>
2020-07-04 20:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
+from flask import Flask, jsonify, request
 import pyrebase
-storage = firebase.storage()
-storage.child("Audio/27 sec clip.wav").download("downloaded.wav")
+import os as os
+
+
+from flask import Flask, jsonify, request
+import pickle
+
+
+config={
+    "apiKey": "AIzaSyDeFLJFr2aqcAK40nZOmBtEDNYij49yyAk",
+    "authDomain": "healdon-916dd.firebaseapp.com",
+    "databaseURL": "https://healdon-916dd.firebaseio.com",
+    "projectId": "healdon-916dd",
+    "storageBucket": "healdon-916dd.appspot.com",
+    "messagingSenderId": "756073662506",
+    "appId": "1:756073662506:web:2f4cb2e5e93f1d4b9d1b53"
+}
+firebase=pyrebase.initialize_app(config)
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+    # get data
+    data = request.get_json(force=True)
+
+    path=os.getcwd()
+    
+    storage = firebase.storage()
+    storage.child("Audio/27 sec clip.wav").download("downloaded.wav")
+    # predictions
+    #result = model.predict(data_df)
+
+    # send back to browser
+    #output = {'results': int(result[0])}
+    output = {'results': path}
+    
+    # return data
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True,use_reloader=False)
